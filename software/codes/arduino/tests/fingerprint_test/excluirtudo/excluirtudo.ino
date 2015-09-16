935ef6769182d7cefc0fb228765305d9bcefa02f
kelvinmaues <kelvinmaus@gmail.com>
2015-09-16 12:08:26 -0300

    Foram adicionados as pastas .zip no diretório libraries, e foi adicionado o diretório de teste fingerprint no diretório tests

---
+++ software/codes/arduino/tests/fingerprint_test/excluirtudo/excluirtudo.ino
@@ -0,0 +1,73 @@
+
+#include <Adafruit_Fingerprint.h>
+#if ARDUINO >= 100
+ #include <SoftwareSerial.h>
+#else
+ #include <NewSoftSerial.h>
+#endif
+
+uint8_t getFingerprintEnroll(uint8_t id);
+
+
+// pin #12 is IN from sensor (GREEN wire)
+// pin #11 is OUT from arduino  (WHITE wire)
+#if ARDUINO >= 100
+SoftwareSerial mySerial(12, 11);
+//#define mySerial Serial1
+#else
+NewSoftSerial mySerial(12, 11);
+#endif
+
+  Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+
+void setup()  
+{
+  Serial.begin(9600);
+  Serial.println("Delete Finger");
+
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1);
+  }
+  Serial.println("Type in the ID # you want delete...");
+  uint8_t id = 0;
+   for ( id=0; id <= 162; id++){
+      Serial.print("deleting ID #");
+      Serial.println(id);
+      deleteFingerprint(id);
+      delay(10);
+   } 
+}
+
+void loop()                     // run over and over again
+{
+  
+  
+}
+
+uint8_t deleteFingerprint(uint8_t id) {
+  uint8_t p = -1;
+  
+  p = finger.deleteModel(id);
+
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Deleted!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return p;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not delete in that location");
+    return p;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return p;
+  } else {
+    Serial.print("Unknown error: 0x"); Serial.println(p, HEX);
+    return p;
+  }   
+}
