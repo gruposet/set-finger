a15ece0fc9c09071f07157e44af7e39c894c984b
gruposet <projetoset@ufpa.br>
2015-09-18 11:32:13 -0300

    atualização do código de teste keypad (correção de pinos). Atualização de diretório de servidor node para o setfinger

---
+++ software/codes/arduino/tests/keypad/keypad.ino
@@ -0,0 +1,35 @@
+/* @file HelloKeypad.pde
+|| @version 1.0
+|| @author Alexander Brevig
+|| @contact alexanderbrevig@gmail.com
+||
+|| @description
+|| | Demonstrates the simplest use of the matrix Keypad library.
+|| #
+*/
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {44, 42, 40, 38}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {36, 34, 32}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+  }
+}
